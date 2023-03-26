04ebe39c11d80dca6425e2bd9b3af26100542854
Fuad <90257746+Fuad-R@users.noreply.github.com>
2023-03-26 14:23:32 +0200

    Create test5.py

---
+++ test5.py
@@ -0,0 +1,65 @@
+import vosk
+import subprocess
+import pyaudio
+import openai
+
+# Initialize Vosk API
+model = vosk.Model("model")
+rec = vosk.KaldiRecognizer(model, 16000)
+
+# Send chatbot intro to OpenAI Chat API
+openai.api_key = "YOUR_API_KEY"
+chatbot_name = "MARVIN"
+chatbot_intro = "Hello MARVIN. You are an AI voice assistant based on Iron Man's JARVIS. My name is Fuad and I created you. You are designed to help me out with my day-to-day tasks. You have a voice and everything you say will be transcribed to live speech."
+chatbot_prompt = f"{chatbot_name}: {chatbot_intro}\nUser:"
+
+response = openai.Completion.create(
+    engine="davinci",
+    prompt=chatbot_prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.65,
+)
+
+# Initialize conversation history
+conversation_history = [
+    {"role": "system", "content": chatbot_intro}
+]
+
+# Wait for "Hey MARVIN"
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        text = rec.Result()
+        if "Hey MARVIN" in text:
+            break
+
+# Transcribe live speech and send to OpenAI Chat API
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        text = rec.Result()
+        print(f"Transcribed speech: {text}")
+        conversation_history.append({"role": "user", "content": text})
+        response = openai.ChatCompletion.create(
+            model="davinci",
+            messages=conversation_history,
+            temperature=0.65,
+        )
+        chatbot_output = response.choices[0].message["content"].strip()
+        conversation_history.append({"role": "assistant", "content": chatbot_output})
+        print(chatbot_output)
+    else:
+        print(rec.PartialResult())
+
+# Output result from OpenAI Chat API into Larynx2
+print(f"Speaking: {chatbot_output}")
+subprocess.run(["/home/pi/larynx2", chatbot_output])
